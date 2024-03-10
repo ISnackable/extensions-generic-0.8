f4c1de8602036518350d407008c575ac14964644
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2024-03-10 08:46:16 +0800

    refactor: Update source description

--- src/ZeroScans/ZeroScans.ts
+++ src/ZeroScans/ZeroScans.ts
@@ -16,7 +16,7 @@ import {
     SearchResultsProviding,
     MangaProviding,
     ChapterProviding,
-    HomePageSectionsProviding
+    HomePageSectionsProviding,
 } from '@paperback/types'
 
 import { Parser } from './parser'
@@ -27,50 +27,60 @@ export const ZeroScansInfo: SourceInfo = {
     version: '2.0.1',
     name: 'Zero Scans',
     icon: 'icon.png',
-    author: 'NmN',
-    authorWebsite: 'http://github.com/pandeynmm',
-    description: 'Extension that pulls manga from bato.to',
+    author: 'Gabe',
+    authorWebsite: 'http://github.com/GabrielCWT',
+    description: 'Extension that pulls manga from zscans.com',
     contentRating: ContentRating.EVERYONE,
     websiteBaseURL: ZEROSCANS_DOMAIN,
     language: 'en',
-    sourceTags: [ 
+    sourceTags: [
         {
             text: 'English',
-            type: BadgeColor.GREY
-            
-        }
+            type: BadgeColor.GREY,
+        },
     ],
-    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED
+    intents:
+        SourceIntents.MANGA_CHAPTERS |
+        SourceIntents.HOMEPAGE_SECTIONS |
+        SourceIntents.CLOUDFLARE_BYPASS_REQUIRED,
 }
 
-const userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1'
+const userAgent =
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1'
 
-export class ZeroScans implements SearchResultsProviding, MangaProviding, ChapterProviding, HomePageSectionsProviding {
+export class ZeroScans
+    implements
+        SearchResultsProviding,
+        MangaProviding,
+        ChapterProviding,
+        HomePageSectionsProviding
+{
     baseUrl = ZEROSCANS_DOMAIN
     requestManager = App.createRequestManager({
         requestsPerSecond: 3,
         requestTimeout: 8000,
         interceptor: {
             interceptRequest: async (request: Request): Promise<Request> => {
-
                 request.headers = {
                     ...(request.headers ?? {}),
                     ...{
                         'user-agent': userAgent,
-                        'referer': `${this.baseUrl}/`
-                    }
+                        referer: `${this.baseUrl}/`,
+                    },
                 }
 
                 return request
             },
 
-            interceptResponse: async (response: Response): Promise<Response> => {
+            interceptResponse: async (
+                response: Response
+            ): Promise<Response> => {
                 return response
-            }
-        }
+            },
+        },
     })
 
-    constructor(private cheerio: CheerioAPI) { }
+    constructor(private cheerio: CheerioAPI) {}
 
     RETRY = 5
     parser = new Parser()
@@ -104,7 +114,10 @@ export class ZeroScans implements SearchResultsProviding, MangaProviding, Chapte
 
         const id_html = $.html().toString()
         const start_index = id_html.indexOf('data:[{details:{id:')
-        let numericId = id_html.substring(start_index+19, id_html.indexOf(',name:'))
+        let numericId = id_html.substring(
+            start_index + 19,
+            id_html.indexOf(',name:')
+        )
 
         if (numericId == 'e') {
             const start_index = id_html.indexOf(',false,"Zero Scans"));') - 5
@@ -120,27 +133,32 @@ export class ZeroScans implements SearchResultsProviding, MangaProviding, Chapte
             json = await this.createChapterRequest(numericId, page)
             chapters.push(...this.parser.parseChapter(json, mangaId, this))
             page += 1
-
         } while (json.data.total > json.data.to)
 
         return chapters
     }
 
-    async createChapterRequest(numericId: string, page: number) : Promise<any> {
-        const url = `${this.baseUrl}/swordflake/comic/${numericId}/chapters?sort=asc&page=${page.toString()}`
+    async createChapterRequest(numericId: string, page: number): Promise<any> {
+        const url = `${
+            this.baseUrl
+        }/swordflake/comic/${numericId}/chapters?sort=asc&page=${page.toString()}`
         const request = App.createRequest({
             url,
             method: 'GET',
         })
-        const response = await this.requestManager.schedule(request, this.RETRY) ?? ''
+        const response =
+            (await this.requestManager.schedule(request, this.RETRY)) ?? ''
         if (!response || !response.data) {
-            return JSON.parse('{error: \'true\'}')
+            return JSON.parse("{error: 'true'}")
         }
         const json = JSON.parse(response.data)
         return json
     }
 
-    async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
+    async getChapterDetails(
+        mangaId: string,
+        chapterId: string
+    ): Promise<ChapterDetails> {
         const request = App.createRequest({
             url: `${this.baseUrl}/swordflake/comic/${mangaId}/chapters/${chapterId}`,
             method: 'GET',
@@ -150,10 +168,16 @@ export class ZeroScans implements SearchResultsProviding, MangaProviding, Chapte
         return this.parser.parseChapterDetails(json, mangaId, chapterId)
     }
 
-
-    async getSearchResults(query: SearchRequest, metadata: any): Promise<PagedResults> {
+    async getSearchResults(
+        query: SearchRequest,
+        metadata: any
+    ): Promise<PagedResults> {
         const page = metadata?.page ?? 1
-        if (page == -1) return App.createPagedResults({ results: [], metadata: { page: -1 } })
+        if (page == -1)
+            return App.createPagedResults({
+                results: [],
+                metadata: { page: -1 },
+            })
 
         const request = App.createRequest({
             url: `${this.baseUrl}/swordflake/comics`,
@@ -169,7 +193,7 @@ export class ZeroScans implements SearchResultsProviding, MangaProviding, Chapte
         })
     }
 
-    async getSearchTags():  Promise<TagSection[]> {
+    async getSearchTags(): Promise<TagSection[]> {
         const request = App.createRequest({
             url: `${this.baseUrl}/swordflake/comics`,
             method: 'GET',
@@ -184,13 +208,22 @@ export class ZeroScans implements SearchResultsProviding, MangaProviding, Chapte
             genres.push(App.createTag({ label: item.name, id: item.slug }))
         }
 
-        const tagSections: TagSection[] = [App.createTagSection({ id: '0', label: 'Genres', tags: genres })]
+        const tagSections: TagSection[] = [
+            App.createTagSection({ id: '0', label: 'Genres', tags: genres }),
+        ]
         return tagSections
     }
 
-    async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults> {
+    async getViewMoreItems(
+        homepageSectionId: string,
+        metadata: any
+    ): Promise<PagedResults> {
         const page = metadata?.page ?? 1
-        if (page == -1) return App.createPagedResults({ results: [], metadata: { page: -1 } })
+        if (page == -1)
+            return App.createPagedResults({
+                results: [],
+                metadata: { page: -1 },
+            })
 
         const request = App.createRequest({
             url: `${this.baseUrl}/swordflake/comics`,
@@ -207,7 +240,9 @@ export class ZeroScans implements SearchResultsProviding, MangaProviding, Chapte
         })
     }
 
-    async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
+    async getHomePageSections(
+        sectionCallback: (section: HomeSection) => void
+    ): Promise<void> {
         let request = App.createRequest({
             url: `${this.baseUrl}/swordflake/home`,
             method: 'GET',
@@ -238,21 +273,21 @@ export class ZeroScans implements SearchResultsProviding, MangaProviding, Chapte
         if (date.includes('LESS THAN AN HOUR') || date.includes('JUST NOW')) {
             time = new Date(Date.now())
         } else if (date.includes('YEAR') || date.includes('YEARS')) {
-            time = new Date(Date.now() - (number * 31556952000))
+            time = new Date(Date.now() - number * 31556952000)
         } else if (date.includes('MONTH') || date.includes('MONTHS')) {
-            time = new Date(Date.now() - (number * 2592000000))
+            time = new Date(Date.now() - number * 2592000000)
         } else if (date.includes('WEEK') || date.includes('WEEKS')) {
-            time = new Date(Date.now() - (number * 604800000))
+            time = new Date(Date.now() - number * 604800000)
         } else if (date.includes('YESTERDAY')) {
             time = new Date(Date.now() - 86400000)
         } else if (date.includes('DAY') || date.includes('DAYS')) {
-            time = new Date(Date.now() - (number * 86400000))
+            time = new Date(Date.now() - number * 86400000)
         } else if (date.includes('HOUR') || date.includes('HOURS')) {
-            time = new Date(Date.now() - (number * 3600000))
+            time = new Date(Date.now() - number * 3600000)
         } else if (date.includes('MINUTE') || date.includes('MINUTES')) {
-            time = new Date(Date.now() - (number * 60000))
+            time = new Date(Date.now() - number * 60000)
         } else if (date.includes('SECOND') || date.includes('SECONDS')) {
-            time = new Date(Date.now() - (number * 1000))
+            time = new Date(Date.now() - number * 1000)
         } else {
             time = new Date(date)
         }
@@ -265,14 +300,16 @@ export class ZeroScans implements SearchResultsProviding, MangaProviding, Chapte
             method: 'GET',
             headers: {
                 'user-agent': userAgent,
-                'referer': `${this.baseUrl}/`
-            }
+                referer: `${this.baseUrl}/`,
+            },
         })
     }
 
     CloudFlareError(status: any) {
         if (status == 503) {
-            throw new Error('CLOUDFLARE BYPASS ERROR:\nPlease go to Settings > Sources > <The name of this source> and press Cloudflare Bypass')
+            throw new Error(
+                'CLOUDFLARE BYPASS ERROR:\nPlease go to Settings > Sources > <The name of this source> and press Cloudflare Bypass'
+            )
         }
     }
 }
